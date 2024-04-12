fff19fb9cc8ec27cee823a45fbd16c4fb30ab0e7
Kushagra Agarwal <140706909+heyitsk@users.noreply.github.com>
2024-04-12 09:55:24 +0000

    conversion of datatype

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,14 @@
+let score = "abc" ;
+console.log(typeof(score));
+let valueInBoolean = Boolean(score);
+console.log(typeof(valueInBoolean));
+console.log(valueInBoolean);
+
+// 2abc > Nan
+// null > 0
+// undefined > Nan
+// true > 1, false > 0
+// 1 > true, 0 > false
+// "" > false, "abc" > true
+
+
