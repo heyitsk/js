80fea9456f9490cb81cd02712531a89417104cd3
Kushagra Agarwal <140706909+heyitsk@users.noreply.github.com>
2024-04-13 06:41:35 +0000

    heap and stack memory done

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -12,3 +12,8 @@
 // Arrays  =>  object  *****
 // Function  =>  function
 // Object  =>  object
+
+
+// stack (primitive), heap (non primitive)
+// this is like call by value and call by reference. when you use primitive datatype it stores it in stack  memeory and it gives the copy into another variable. if change is made in 2nd var no change is made in 1st
+// heap memory gives the refernce each time so a change made in second variable changes the 1st variable 
